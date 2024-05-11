383ef26114caffe84552395f0cdb8f69e51bc930
locolin1204 <locolin99@gmail.com>
2024-05-11 23:15:56 +0800

    chore: update correlation matrix

--- datavisualisation.py
+++ datavisualisation.py
@@ -7,9 +7,10 @@ import message as msg
 from dython.nominal import identify_nominal_columns, associations
 
 
-def corr_matrix_visualize(df):
-    categorical_features = identify_nominal_columns(df)
-    complete_correlation = associations(df, filename='complete_correlation.png', figsize=(10, 10))
+def corr_matrix_visualize(data, filename):
+    categorical_features = identify_nominal_columns(data)
+    print("categorical features: ", categorical_features)
+    complete_correlation = associations(data, filename=f'{filename}.png', figsize=(10, 10))
     df_complete_corr = complete_correlation['corr']
     df_complete_corr.dropna(axis=1, how='all').dropna(axis=0, how='all').style.background_gradient(cmap='coolwarm', axis=None)
 
@@ -23,8 +24,22 @@ def matrix_scatter_visualize():
 
 
 df = pd.read_csv("./data/car_prices.csv")
+
+# preprocess data
 msg.message("[INFO] Start preprocessing data...")
 df = pp.car_preprocessing(df)
 
-corr_matrix_visualize(df)
+# visualize correlation matrix
+msg.message("[INFO] Start visualizing correlation matrix...")
+corr_matrix_visualize(df, "corr_matrix_visualize")
+msg.message("[INFO] Visualize completed.")
 
+# drop similar columns, >=0.99
+drop_col = ["make"]
+df.drop(drop_col, axis="columns", inplace=True)
+msg.message(f"[INFO] Dropped col: {drop_col}")
+
+# visualize correlation matrix
+msg.message("[INFO] Start visualizing correlation matrix...")
+corr_matrix_visualize(df, "corr_matrix_visualize_min")
+msg.message("[INFO] Visualize completed.")
